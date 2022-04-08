8afed67021fb6341f7bb53229e50311dc55ffd85
batuhannzorlu <batuhanzorlu1995@outlook.com>
2022-04-08 12:35:30 -0400

    bz_queue implemented by using 2 bz_stacks

--- my_stl/bz_queue.h
+++ my_stl/bz_queue.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "bz_list.h"
+#include "bz_stack.h"
 
 template <typename T>
 
@@ -13,11 +13,18 @@ public:
 
 	T front();
 	T back();
-	void push_back();
+	void push_back(T value);
 	void pop_front();
 
+	bool empty();
+
 private:
-	bz_list<T> private_q;
+	bz_stack<T> private_front;
+	bz_stack<T> private_back;
+
+	/*If back stack is empty, will shift all the values from front stack to back stack.*/
+	void shifting();
+	T back_val=NULL;
 };
 
 template<typename T>
@@ -28,19 +35,53 @@ bz_queue<T>::bz_queue() {
 
 template<typename T>
 T bz_queue<T>:: front() {
+		
+	if (this->private_back.empty() == true && this->private_front.empty() == false)
+		this->shifting();
+
+	return this->private_back.top();
 
 }
 
 template<typename T>
 T bz_queue<T> :: back() {
-
+	return back_val;
+	
+		
 }
 
 template<typename T>
-void bz_queue<T>::push_back() {
-
+void bz_queue<T>::push_back(T value) {
+		
+	this->private_front.push(value);
+	back_val = value;
 }
 template<typename T>
 void bz_queue<T>::pop_front() {
+	
+	if (this->private_back.empty() == true && this->private_front.empty() == false)
+		this->shifting();
+		
+	this->private_back.pop();
+	
+}
 
+
+template<typename T>
+bool bz_queue<T>::empty() {
+
+	if (this->private_back.empty() == true && this->private_front.empty() == true)
+		return true;
+
+	return false;
+
+}
+
+template<typename T>
+void bz_queue<T> ::shifting() {
+
+		while (!this->private_front.empty()) {
+			this->private_back.push(this->private_front.top());
+			this->private_front.pop();
+		}
 }
